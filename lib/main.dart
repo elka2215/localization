41f5d95b8e199bac79a438a3856514091c534b5f
Admin <Orazgaliev99@mail.ru>
2022-06-10 18:10:40 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:untitled3/screens/login_screen.dart';
+
+import 'generated/l10n.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Flutter Demo",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      locale: const Locale('ru', 'RU'),
+      supportedLocales: S.delegate.supportedLocales,
+      home: const LoginScreen(),
+    );
+  }
+}
